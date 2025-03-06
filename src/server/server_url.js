c5c65591b68eb1ce65360067a0603c1f739365a8
Andres Vaca <152392671+AndresVacaPiramide69@users.noreply.github.com>
2025-03-06 09:54:34 +0100

    Update server_url.js

--- src/server/server_url.js
+++ src/server/server_url.js
@@ -1,2 +1,3 @@
-export const URL_SERVER = 'https://gorv71bzzh.execute-api.us-east-1.amazonaws.com/suma'
+//export const URL_SERVER = 'https://gorv71bzzh.execute-api.us-east-1.amazonaws.com/suma'
+export const URL_SERVER = 'https://5y8sz1xrp5.execute-api.us-east-1.amazonaws.com/back'
 // export const URL_SERVER = 'http://localhost:3000'
